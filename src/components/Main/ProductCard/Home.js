c26c8e92be59253bdcd05a9bf1f80f247bce5c14
Andrii Reverchuk <andrejrevercuk@gmail.com>
2021-07-15 10:25:08 +0300

    add routing

--- src/components/Main/ProductCard/Home.js
+++ src/components/Main/ProductCard/Home.js
@@ -8,7 +8,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import { Button } from '@material-ui/core';
-const drawerWidth = 180;
 
 const useStyles = makeStyles((theme) => ({
    cardWidth: {
